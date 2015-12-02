1ae3b773468617d0ca41b9e655192ec10ba097b8
Tomasz Prokop <tomasz.prokop@lumesse.com>
2015-12-02 11:44:59 +0100

    FragmentServlet

---
+++ src/main/java/pack/MyServlet.java
@@ -0,0 +1,19 @@
+package pack;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("This response is generated from the Plugged-in servlet domel 12345");
+        resp.getWriter().flush();
+    }
+}
