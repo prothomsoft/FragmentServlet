f078e21accb74b5d8fa1fa95bddd3302df25397e
Tomasz Prokop <tomasz.prokop@lumesse.com>
2015-12-02 13:31:23 +0100

    WebFragment servlet with annotation

---
+++ src/main/java/pack/SecondServlet.java
@@ -0,0 +1,21 @@
+package pack;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("This response is generated from the Plugged-in second servlet annotated");
+        resp.getWriter().flush();
+    }
+}
